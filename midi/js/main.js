c4ca1eee6a4f2f2e416d3384a347967b62aa4782
phil jones <interstar@gmail.com>
2014-02-01 00:23:53 -0200

    midi is no longer a submodule but my copy of stuff that works

---
+++ midi/js/main.js
@@ -0,0 +1,23 @@
+var scheduler;
+
+window.onload = function () {
+    scheduler = new SchedulerObject(50);
+    noteCalculator.setCurrent("major");      
+
+    var game = new Game("canvasId",MIDI);
+    scheduler.add(game);
+    
+	MIDI.loadPlugin({
+		soundfontUrl: "./soundfont/",
+		instrument: "acoustic_grand_piano",
+
+		callback: function() {            		    
+            scheduler.start();
+            MIDI.setVolume(0,127)
+            MIDI.noteOn(0,58,50,0)
+            MIDI.noteOff(0,58,0.75)                
+ 
+		}
+	});
+};
+
